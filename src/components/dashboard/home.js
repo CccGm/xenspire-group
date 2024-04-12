395cae0296b310fd69e3f7d4016e362172d51438
Ccc GM <mihirghelani.cyber@gmail.com>
2024-04-12 20:36:56 +0530

    added stepper

--- src/components/dashboard/home.js
+++ src/components/dashboard/home.js
@@ -3,6 +3,11 @@ import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import Typography from "@mui/material/Typography";
+import StepConnector, {
+  stepConnectorClasses,
+} from "@mui/material/StepConnector";
+import { RadioButtonChecked } from "@mui/icons-material";
+import { styled } from "@mui/material/styles";
 import { My_Experience } from "./my_experience";
 import { My_Information } from "./my_information";
 import { Upload_Document } from "./upload_document";
@@ -17,48 +22,9 @@ const steps = [
 
 export function Home() {
   const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
-
-  const isStepOptional = (step) => {
-    return step === 1;
-  };
-
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
-  };
 
   const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleSkip = () => {
-    if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.");
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
   };
 
   const Show_Fragment = () => {
@@ -76,24 +42,109 @@ export function Home() {
     }
   };
 
+  const QontoConnector = styled(StepConnector)(({ theme }) => ({
+    [`&.${stepConnectorClasses.alternativeLabel}`]: {
+      top: 10,
+      left: "calc(-50% + 16px)",
+      right: "calc(50% + 16px)",
+    },
+    [`&.${stepConnectorClasses.active}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        borderColor: "#53783B",
+      },
+    },
+    [`&.${stepConnectorClasses.completed}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        borderColor: "#53783B",
+      },
+    },
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor:
+        theme.palette.mode === "dark" ? theme.palette.grey[800] : "#eaeaf0",
+      borderTopWidth: 3,
+      borderRadius: 1,
+    },
+  }));
+
+  const QontoStepIconRoot = styled("div")(({ theme, ownerState }) => ({
+    color: theme.palette.mode === "dark" ? theme.palette.grey[700] : "#eaeaf0",
+    display: "flex",
+    height: 22,
+    alignItems: "center",
+    ...(ownerState.active && {
+      color: "#568ec9",
+    }),
+    "& .QontoStepIcon-completedIcon": {
+      color: "#53783B",
+      zIndex: 1,
+      fontSize: 18,
+    },
+    "& .QontoStepIcon-circle": {
+      width: 8,
+      height: 8,
+      borderRadius: "50%",
+      backgroundColor: "currentColor",
+    },
+  }));
+
+  function QontoStepIcon(props) {
+    const { active, completed, className } = props;
+
+    return (
+      <QontoStepIconRoot ownerState={{ active }} className={className}>
+        {completed ? (
+          <RadioButtonChecked color="success" />
+        ) : (
+          <RadioButtonChecked />
+        )}
+      </QontoStepIconRoot>
+    );
+  }
+
   return (
     <div className="pb-20">
       <div className="py-10">
-        <Stepper activeStep={activeStep} alternativeLabel>
+        <Stepper
+          activeStep={activeStep}
+          alternativeLabel
+          connector={<QontoConnector />}>
           {steps.map((label, index) => {
-            const stepProps = {};
             const labelProps = {};
-            if (isStepOptional(index)) {
+
+            if (index === 0) {
               labelProps.optional = (
-                <Typography variant="caption">your Experiences</Typography>
+                <Typography variant="caption" sx={{ color: "#7B964A" }}>
+                  Name and Email
+                </Typography>
               );
             }
-            if (isStepSkipped(index)) {
-              stepProps.completed = false;
+            if (index === 1) {
+              labelProps.optional = (
+                <Typography variant="caption" sx={{ color: "#7B964A" }}>
+                  Your Experiences
+                </Typography>
+              );
             }
+            if (index === 2) {
+              labelProps.optional = (
+                <Typography variant="caption" sx={{ color: "#7B964A" }}>
+                  Start Collaborating
+                </Typography>
+              );
+            }
+            if (index === 3) {
+              labelProps.optional = (
+                <Typography variant="caption" sx={{ color: "#7B964A" }}>
+                  Automatic Sharing
+                </Typography>
+              );
+            }
+
             return (
-              <Step key={label} {...stepProps}>
-                <StepLabel {...labelProps}>{label}</StepLabel>
+              <Step key={label}>
+                <StepLabel StepIconComponent={QontoStepIcon} {...labelProps}>
+                  {label}
+                </StepLabel>
               </Step>
             );
           })}
